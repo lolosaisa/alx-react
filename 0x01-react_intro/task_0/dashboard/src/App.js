509d344eafa3d9b72f7e934f4f79a0c815087804
lolo saisa <lwamaitha3924@gmail.com>
2024-07-24 17:14:56 +0300

    styling added

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,11 +1,11 @@
-//import logo from './logo.svg';
+import logo from './holberton-logo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <img src={logo} className="App-logo" alt="logo" />
         <h1>School dashboard</h1>
       </header>
       <body className='App-body'> 
